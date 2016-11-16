cfa5935d2a0e924eb9e0b27d05f40c220ee1f2cf
Dmitry Danileyko <danileyko@i.ua>
2016-11-16 16:16:24 +0200

    pasha-1

--- three_cats/apps/elephants/templatetags/elephants.py
+++ three_cats/apps/elephants/templatetags/elephants.py
@@ -1,6 +1,8 @@
 from django import template
+from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
 
-from apps.elephants.models import Sizes
+from apps.elephants.models import Sizes, Categories
 
 
 register = template.Library()
@@ -14,3 +16,29 @@ def get_size_desc(size_id):
         return ''
 
     return size.description
+
+
+@register.simple_tag
+def get_categories():
+    try:
+        categories = Categories.objects.all().order_by('sequence')
+        m = ''
+        for category in categories:
+            m = m + '<li class="nav-item"><a class="nav-link cc-main-menu-color" href="/showcase/%i/" title="%s">%s</a></li>' % (category.id, _(category.name), _(category.name))
+
+    except:
+        return ''
+
+    return mark_safe(m)
+
+
+@register.simple_tag
+def sizes_count(sizes):
+    try:
+        n = 0
+        for size in sizes:
+            n = n + 1
+    except:
+        return n
+
+    return n
