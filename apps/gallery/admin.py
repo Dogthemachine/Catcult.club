e408b6e9fe357eec0fd983e82cc0c9a1fc23a34c
Dmitry Danileyko <danileyko@i.ua>
2018-06-01 11:55:23 +0300

    gallery empty name

--- apps/gallery/admin.py
+++ apps/gallery/admin.py
@@ -6,22 +6,7 @@ from apps.gallery.models import Gallery
 
 
 class GalleryAdmin(TranslationAdmin):
-    fieldsets = [
-        (u'Gallery', {'fields': ('name', 'image', 'description')})
-    ]
-
-    class Media:
-        js = [
-            '/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
-            '/static/grappelli/tinymce_setup/tinymce_setup.js',
-            '/static/modeltranslation/js/force_jquery.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js',
-            '/static/modeltranslation/js/tabbed_translation_fields.js',
-        ]
-
-        css = {
-            'screen': ('/static/modeltranslation/css/tabbed_translation_fields.css',),
-        }
+    list_display = ('added', 'name')
 
 
 admin.site.register(Gallery, GalleryAdmin)
--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Gallery(models.Model):
-    name = models.CharField(_('name'), max_length=250)
+    name = models.CharField(_('name'), max_length=250, blank=True, default='')
     image = ResizedImageField(size=[2500, 2500], upload_to='photos/%Y/%m/%d')
     image_small = ResizedImageField(size=[300, 300], crop=['middle', 'center'], upload_to='small_photos/%Y/%m/%d', editable=False)
     description = models.TextField(_('description'), blank=True, default='')
