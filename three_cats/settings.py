0199aab1f692622daef3eede16a462a20e8a929f
Dmitry Danileyko <danileyko@i.ua>
2020-06-28 14:25:29 +0300

    cache item

--- three_cats/settings.py
+++ three_cats/settings.py
@@ -82,6 +82,16 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:" + os.environ.get('TC_REDIS_PORT')+"/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 WSGI_APPLICATION = 'three_cats.wsgi.application'
 
 DATABASES = {
