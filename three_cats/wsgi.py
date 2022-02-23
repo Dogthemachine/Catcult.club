583f7f33bcddc05d0b4f7f4143ba2f626d59aaad
Dogthemachine <danileyko@gmail.com>
2022-02-23 16:30:26 +0200

    locating env file for uwsgi

--- three_cats/wsgi.py
+++ three_cats/wsgi.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import dotenv
-
-dotenv.read_dotenv()
-
 from django.core.wsgi import get_wsgi_application
+
+# dotenv.read_dotenv()
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname('/sites/Catcult.club/')), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "three_cats.settings")
 
 application = get_wsgi_application()
+
